e1a32bef6b74f355f0f9ca2f68cdf42714ef094d
이상철 <zlrz6706@gmail.com>
2021-09-26 23:08:34 +0900

    모든 state 값을 부모컴포넌트로 옮겨서 props로 자식에게 뿌린 상태

--- src/Components/Children.js
+++ src/Components/Children.js
@@ -3,46 +3,18 @@ import Parents from "./Parents";
 import "../../src/main.css"
 
 class Children extends React.Component {
-
-  constructor() {
-    super ()
-
-    this.state={
-        isSwitchOn: false,
-      };
-  }
-
-  // 토글스위치 함수를 boolean 값에 맞춰 변하게 할 수 있도록 조건문으로 수정
-  toggleSwitch = () => {
-    // if(this.state.isSwitchOn) {
-    //   this.setState({
-    //     isSwitchOn:false
-    //   })
-    // }else {
-    //   this.setState({
-    //     isSwitchOn:true
+  
+    render() {
     
-    //   });
-    this.setState({
-      isSwitchOn: !this.state.isSwitchOn,
-    })
+    console.log("props >>>",this.props)
 
-  }
-
-  render() {
-    //  스위치가 켜졌으면 색깔이 파란색
-    //  스위치가 꺼졌으면 색깔이 검은색
-    // 이럴땐 데이터 타입을 boolean을 사용해주는 게 좋다.
-    console.log(this.state)
-
-    const { isSwitchOn: 스위치, } = this.state //구조분해할당 
   
     return (
       <div>
-      <h1 className={스위치 ? "switchOn"  : "switchOff" }>
-        Switch: {스위치 ? "On" : "Off"} 
+      <h1 className={this.props.isSwitchOn ? "switchOn"  : "switchOff" }>
+        Switch: {this.props.isSwitchOn ? "On" : "Off"} 
         </h1>
-        <button onClick={this.toggleSwitch}>toggle </button>
+        <button onClick={this.props.toggleSwitch}>toggle </button>
         </div>
       )  
     };
--- src/Components/Parents.js
+++ src/Components/Parents.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import Children from "./Children";
 
 class Parents extends Component {
-    
+
+  constructor() {
+    super ()
+
+    this.state={
+        isSwitchOn: false,
+      };
+  }
+
+  toggleSwitch = () => {
+    this.setState({
+      isSwitchOn: !this.state.isSwitchOn,
+    })
+
+  }
+
   render() {
     return (
-      <Children />
+      <Children 
+      isSwitchOn={this.state.isSwitchOn}
+      toggleSwitch={this.toggleSwitch}/>
     );
   }
 }
