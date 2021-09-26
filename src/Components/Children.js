216f12eac0313b7d3b77f317481d4429ffc35ca5
이상철 <zlrz6706@gmail.com>
2021-09-26 22:48:11 +0900

    state 와 이벤트핸들러 사용해서 값 변화시키기 예제코드 및 정리

--- src/Components/Children.js
+++ src/Components/Children.js
@@ -8,18 +8,43 @@ class Children extends React.Component {
     super ()
 
     this.state={
-      titleColor: "red",
-      titleSize: "50px",
+        isSwitchOn: false,
       };
   }
 
+  // 토글스위치 함수를 boolean 값에 맞춰 변하게 할 수 있도록 조건문으로 수정
+  toggleSwitch = () => {
+    // if(this.state.isSwitchOn) {
+    //   this.setState({
+    //     isSwitchOn:false
+    //   })
+    // }else {
+    //   this.setState({
+    //     isSwitchOn:true
+    
+    //   });
+    this.setState({
+      isSwitchOn: !this.state.isSwitchOn,
+    })
+
+  }
+
   render() {
+    //  스위치가 켜졌으면 색깔이 파란색
+    //  스위치가 꺼졌으면 색깔이 검은색
+    // 이럴땐 데이터 타입을 boolean을 사용해주는 게 좋다.
+    console.log(this.state)
+
+    const { isSwitchOn: 스위치, } = this.state //구조분해할당 
+  
     return (
-      <h1 style={{color : this.state.titleColor,
-                  fontSize: this.state.titleSize}}>
-        Hello world 
+      <div>
+      <h1 className={스위치 ? "switchOn"  : "switchOff" }>
+        Switch: {스위치 ? "On" : "Off"} 
         </h1>
-      )
+        <button onClick={this.toggleSwitch}>toggle </button>
+        </div>
+      )  
     };
   }
 
